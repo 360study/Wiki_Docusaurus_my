885ac39bc32092e781df7d54a9313d683392fd20
jx398 <35520791+jx398@users.noreply.github.com>
2023-02-28 09:24:04 +0800

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,77 +22,77 @@ const config = {
   //  'https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/embed.js',
   //  'https://cos.ap-guangzhou.myqcloud.com/wiki-media-1253965369/doc/autoFitIframe.js'
   //],
-  // plugins: [
-  //   [
-  //     require.resolve("@cmfcmf/docusaurus-search-local"),
-  //     {
-  //         // whether to index docs pages
-  //         indexDocs: true,
+  plugins: [
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+          // whether to index docs pages
+          indexDocs: true,
 
-  //         // Whether to also index the titles of the parent categories in the sidebar of a doc page.
-  //         // 0 disables this feature.
-  //         // 1 indexes the direct parent category in the sidebar of a doc page
-  //         // 2 indexes up to two nested parent categories of a doc page
-  //         // 3...
-  //         //
-  //         // Do _not_ use Infinity, the value must be a JSON-serializable integer.
-  //         indexDocSidebarParentCategories: 0,
+          // Whether to also index the titles of the parent categories in the sidebar of a doc page.
+          // 0 disables this feature.
+          // 1 indexes the direct parent category in the sidebar of a doc page
+          // 2 indexes up to two nested parent categories of a doc page
+          // 3...
+          //
+          // Do _not_ use Infinity, the value must be a JSON-serializable integer.
+          indexDocSidebarParentCategories: 0,
 
-  //         // whether to index blog pages
-  //         indexBlog: true,
+          // whether to index blog pages
+          indexBlog: true,
 
-  //         // whether to index static pages
-  //         // /404.html is never indexed
-  //         indexPages: false,
+          // whether to index static pages
+          // /404.html is never indexed
+          indexPages: false,
 
-  //         // language of your documentation, see next section
-  //         language: "zh",
+          // language of your documentation, see next section
+          language: "zh",
 
-  //         // setting this to "none" will prevent the default CSS to be included. The default CSS
-  //         // comes from autocomplete-theme-classic, which you can read more about here:
-  //         // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
-  //         // When you want to overwrite CSS variables defined by the default theme, make sure to suffix your
-  //         // overwrites with `!important`, because they might otherwise not be applied as expected. See the
-  //         // following comment for more information: https://github.com/cmfcmf/docusaurus-search-local/issues/107#issuecomment-1119831938.
-  //         style: undefined,
+          // setting this to "none" will prevent the default CSS to be included. The default CSS
+          // comes from autocomplete-theme-classic, which you can read more about here:
+          // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
+          // When you want to overwrite CSS variables defined by the default theme, make sure to suffix your
+          // overwrites with `!important`, because they might otherwise not be applied as expected. See the
+          // following comment for more information: https://github.com/cmfcmf/docusaurus-search-local/issues/107#issuecomment-1119831938.
+          style: undefined,
 
-  //         // The maximum number of search results shown to the user. This does _not_ affect performance of
-  //         // searches, but simply does not display additional search results that have been found.
-  //         maxSearchResults: 8,
+          // The maximum number of search results shown to the user. This does _not_ affect performance of
+          // searches, but simply does not display additional search results that have been found.
+          maxSearchResults: 8,
 
-  //         // lunr.js-specific settings
-  //         lunr: {
-  //           // When indexing your documents, their content is split into "tokens".
-  //           // Text entered into the search box is also tokenized.
-  //           // This setting configures the separator used to determine where to split the text into tokens.
-  //           // By default, it splits the text at whitespace and dashes.
-  //           //
-  //           // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
-  //           tokenizerSeparator: /[\s\-]+/,
-  //           // https://lunrjs.com/guides/customising.html#similarity-tuning
-  //           //
-  //           // This parameter controls the importance given to the length of a document and its fields. This
-  //           // value must be between 0 and 1, and by default it has a value of 0.75. Reducing this value
-  //           // reduces the effect of different length documents on a term’s importance to that document.
-  //           b: 0.75,
-  //           // This controls how quickly the boost given by a common word reaches saturation. Increasing it
-  //           // will slow down the rate of saturation and lower values result in quicker saturation. The
-  //           // default value is 1.2. If the collection of documents being indexed have high occurrences
-  //           // of words that are not covered by a stop word filter, these words can quickly dominate any
-  //           // similarity calculation. In these cases, this value can be reduced to get more balanced results.
-  //           k1: 1.2,
-  //           // By default, we rank pages where the search term appears in the title higher than pages where
-  //           // the search term appears in just the text. This is done by "boosting" title matches with a
-  //           // higher value than content matches. The concrete boosting behavior can be controlled by changing
-  //           // the following settings.
-  //           titleBoost: 5,
-  //           contentBoost: 1,
-  //           tagsBoost: 3,
-  //           parentCategoriesBoost: 2, // Only used when indexDocSidebarParentCategories > 0
-  //         }     
-  //    },
-  //   ],
-  // ],
+          // lunr.js-specific settings
+          lunr: {
+            // When indexing your documents, their content is split into "tokens".
+            // Text entered into the search box is also tokenized.
+            // This setting configures the separator used to determine where to split the text into tokens.
+            // By default, it splits the text at whitespace and dashes.
+            //
+            // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
+            tokenizerSeparator: /[\s\-]+/,
+            // https://lunrjs.com/guides/customising.html#similarity-tuning
+            //
+            // This parameter controls the importance given to the length of a document and its fields. This
+            // value must be between 0 and 1, and by default it has a value of 0.75. Reducing this value
+            // reduces the effect of different length documents on a term’s importance to that document.
+            b: 0.75,
+            // This controls how quickly the boost given by a common word reaches saturation. Increasing it
+            // will slow down the rate of saturation and lower values result in quicker saturation. The
+            // default value is 1.2. If the collection of documents being indexed have high occurrences
+            // of words that are not covered by a stop word filter, these words can quickly dominate any
+            // similarity calculation. In these cases, this value can be reduced to get more balanced results.
+            k1: 1.2,
+            // By default, we rank pages where the search term appears in the title higher than pages where
+            // the search term appears in just the text. This is done by "boosting" title matches with a
+            // higher value than content matches. The concrete boosting behavior can be controlled by changing
+            // the following settings.
+            titleBoost: 5,
+            contentBoost: 1,
+            tagsBoost: 3,
+            parentCategoriesBoost: 2, // Only used when indexDocSidebarParentCategories > 0
+          }     
+     },
+    ],
+  ],
   themes: [
     // ... Your other themes.
     /*
@@ -183,11 +183,11 @@ const config = {
 
       algolia: {
 
-        apiKey: '5c07d8bf9c9928c4453857f6cad0420e',//?02bd2340879bdb682f2a9fe509fec240
+        apiKey: '3c9c3aadf0226445bcc782076a31f5b9',//?02bd2340879bdb682f2a9fe509fec240
         indexName: 'wiki-power', //
 
         // The application ID provided by Algolia
-        appId: 'BH4D9OD16A', //BH4D9OD16A 是默认的，文档见 https://autocomplete-experimental.netlify.app/docs/docsearchmodal/#appid，用自己的 ID IRO903CONI 反而搜不出内容 
+        appId: 'NKV74XMM46', //BH4D9OD16A 是默认的，文档见 https://autocomplete-experimental.netlify.app/docs/docsearchmodal/#appid，用自己的 ID IRO903CONI 反而搜不出内容 
 
         // Public API key: it is safe to commit it
         // apiKey: 'defe7fd8690822eed8e3c94801bab286',
